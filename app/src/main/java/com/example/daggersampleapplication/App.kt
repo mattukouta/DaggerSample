a028483abffff30ca8fd3558651b222ad2af5fdf
Kouta Takamatsu <bb1016181@gmail.com>
2020-05-03 03:43:24 +0900

    空白の追加

--- app/src/main/java/com/example/daggersampleapplication/App.kt
+++ app/src/main/java/com/example/daggersampleapplication/App.kt
@@ -4,7 +4,7 @@ import dagger.android.AndroidInjector
 import dagger.android.DaggerApplication
 
 class App : DaggerApplication() {
-    override fun applicationInjector(): AndroidInjector<out DaggerApplication> {
+    override fun applicationInjector() : AndroidInjector<out DaggerApplication> {
         return DaggerAppComponent.factory().create(this)
     }
 }
